b0770324cb1ab039dc3c04c8f57f09ed5e1ae13b
onowahoo <onowahoo@gmail.com>
2015-09-21 18:21:31 -0400

    ios variable scope

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -16,13 +16,50 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSArray *stock = @[ @"Swizzle Soda",
+                        @"Mr. Fizz",
+                        @"Buzz Doctor",
+                        @"De Bug Beer",
+                        @"MACHINE",
+                        @"Liquid Ice"     ];
+    NSUInteger theCount = [self countOfUppercaseStringsInArray:stock];
+    NSLog(@"%lu",theCount);
+    
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    NSMutableArray *newMutableArray = [[NSMutableArray alloc] init];
+    newMutableArray = [array mutableCopy];
+    [newMutableArray addObject:string];
+    return newMutableArray;
+}
+
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    NSUInteger upperCount = 0;
+    
+    for (NSUInteger i=0; i < [array count]; i++) {
+        NSString *arrayObject = array[i];
+        NSString *upperObject = [arrayObject uppercaseString];
+        if ([arrayObject isEqualToString:upperObject]) {
+            upperCount ++;
+            
+        }
+        
+    }
+    
+    return upperCount;
+}
+
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    
+    [array removeAllObjects];
+    
+    for (NSUInteger i=0; i < [array count]; i++) {
+        
+    }
+
+}
 
 @end

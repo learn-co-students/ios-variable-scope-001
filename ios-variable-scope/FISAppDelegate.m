f1cb7f74da0f7bffaf009966ab7a4b43eebe0ab3
Diego-F-Aguirre <diego3884@gmail.com>
2015-11-01 20:33:40 -0500

    variable-scope complete-001

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,46 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array{
+    
+    NSMutableArray *newArray = [array mutableCopy];
+    [newArray addObject:string];
+    
+    return newArray;
+}
+
+// Given an array return the number of uppercase strings
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array{
+    
+
+    //  Create an example array , when checked this array should return a NSUInteger of 4 capitlized strings
+    // NSArray *newArray = @[@"ONE", @"TWO",@"THREE",@"FOUR"]; // should return 4
+
+    
+    //Create a counter which will increment if string is capitilized
+    NSUInteger counter = 0;
+    
+    // Iterate over each item in the array
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        
+        // Inside the loop create an uppercase string from the original string
+        NSString *copiedString = [array[i] uppercaseString];
+        
+        // Once I have an item I want to check if it's uppercase
+        // Compare the strings and if the result is True increment counter
+        if ([copiedString isEqualToString:array[i]]) {
+            // If uppercase add to the counter
+            counter++;
+        }
+    }
+    NSLog(@"%lu",(unsigned long)counter);
+    // return the counter
+    return counter;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array{
+    
+    [array removeAllObjects];
+}
 
 @end
